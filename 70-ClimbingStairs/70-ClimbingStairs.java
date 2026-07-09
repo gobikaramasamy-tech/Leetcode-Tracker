384c7e9c46de6e829e653e7d4f69743972a8a0f4
gobikaramasamy-tech <gobikaramasamy21@gmail.com>
2026-07-09 15:19:11 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 3:19:10 PM
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return helper(n,dp); 
+    }
+
+    int helper(int n,int dp[]){
+        if(n<=2){
+            dp[n] = n;
+            return n;
+        }
+        if(dp[n] != -1) return dp[n];
+        dp[n]= helper(n-1,dp)+helper(n-2,dp);
+        return dp[n];
+    }
+}
