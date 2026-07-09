2a607d0ef0af90cf9245e10a6db8edd3611ae2c6
gobikaramasamy-tech <gobikaramasamy21@gmail.com>
2026-07-09 15:13:20 +0530

    Create 2955-AccountBalanceAfterRoundedPurchase

---
+++ 2955-AccountBalanceAfterRoundedPurchase/2955-AccountBalanceAfterRoundedPurchase.java
@@ -0,0 +1,13 @@
+// Last updated: 7/9/2026, 3:13:20 PM
+class Solution {
+    public int accountBalanceAfterPurchase(int amt) {
+        if(amt%10 == 0)
+        {
+            return 100-amt;
+        }
+        int ans=100-amt;
+        return amt%10 >= 5 ? (ans/10)*10 : ((ans/10)+1)*10;
+    }
+}
+        
+    
