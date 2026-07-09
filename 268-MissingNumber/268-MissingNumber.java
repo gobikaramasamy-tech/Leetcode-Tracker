5dd6731cb342cc00c6ebc80b133211efb7627b06
gobikaramasamy-tech <gobikaramasamy21@gmail.com>
2026-07-09 15:16:32 +0530

    Create 268-MissingNumber

---
+++ 268-MissingNumber/268-MissingNumber.java
@@ -0,0 +1,13 @@
+// Last updated: 7/9/2026, 3:16:32 PM
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor =0;
+        for(int i=0;i<=nums.length;i++){
+            xor = xor^i;
+        }
+        for(int x:nums){
+            xor=xor^x;
+        }
+        return xor;
+    }
+}
