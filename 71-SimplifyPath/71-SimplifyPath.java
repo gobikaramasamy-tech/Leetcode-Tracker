eb847c53e3c4bb51499aad2355ccbcb34a09a99a
gobikaramasamy-tech <gobikaramasamy21@gmail.com>
2026-07-09 15:19:09 +0530

    Create 71-SimplifyPath

---
+++ 71-SimplifyPath/71-SimplifyPath.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 3:19:09 PM
+class Solution {
+    public String simplifyPath(String path) {
+        Deque<String> dirOrFiles = new ArrayDeque<>();
+        for (String dirOrFile : path.split("/")) {
+            if (!dirOrFiles.isEmpty() && dirOrFile.equals("..")) {
+                dirOrFiles.removeLast();
+            } else if (!dirOrFile.equals(".") && !dirOrFile.equals("") && !dirOrFile.equals("..")) {
+                dirOrFiles.addLast(dirOrFile);
+            }
+        }
+        StringBuilder simplified_path = new StringBuilder();
+        for (String dirOrFile : dirOrFiles) {
+            simplified_path.append("/").append(dirOrFile);
+        }
+        return simplified_path.length() == 0 ? "/" : simplified_path.toString();
+    }
+}
