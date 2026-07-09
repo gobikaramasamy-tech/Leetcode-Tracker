ebf09a0f308f7ee0d0f510e1ec89e06cd129afb0
gobikaramasamy-tech <gobikaramasamy21@gmail.com>
2026-07-09 15:18:01 +0530

    Create 123-BestTimeToBuyAndSellStockIii

---
+++ 123-BestTimeToBuyAndSellStockIii/123-BestTimeToBuyAndSellStockIii.java
@@ -0,0 +1,13 @@
+// Last updated: 7/9/2026, 3:18:00 PM
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy1 = -prices[0], buy2 = -prices[0], sell1 = 0, sell2 = 0;
+        for(int price : prices) {        
+            buy1 = Math.max(buy1, -price);
+            sell1 = Math.max(sell1, buy1 + price);
+            buy2 = Math.max(buy2, sell1 - price);
+            sell2 = Math.max(sell2, buy2 + price);
+        }
+        return sell2;
+    }
+}
